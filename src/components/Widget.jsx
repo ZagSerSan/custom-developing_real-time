552f6c0bf86b9af949d79343920db07239986a37
ZagSerSan <yan.steys1997@gmail.com>
2023-07-12 09:36:27 +0200

    proces css

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef, useState } from 'react'
 import './widget.css'
 import {getWeather} from '../services/http.service'
+import search_icon from '../assets/search.svg'
 
 const Widget = () => {
   const [weatherData, setWeatherData] = useState(null)
@@ -15,12 +16,28 @@ const Widget = () => {
   }
 
   return (
-    <div className='wrapper'>
-      <div className="search">
+    <div className='search'>
+      <div className="search-place">
         <input ref={sityInput} type="text" defaultValue='London'/>
-        {weatherData && <img src={weatherData.icon_src} />}
-        <button onClick={getValue} >search</button>
+        <button onClick={getValue}>
+          <img src={search_icon} alt="Найти" />
+        </button>
       </div>
+      {weatherData && (
+        <div className="info">
+          <img src={weatherData.icon_src} />
+          <p className="info__cels">
+            {weatherData.main.temp.toString().slice(0,2)} °C
+          </p>
+          <p className="info__descrip">
+            {weatherData.weather[0].description}
+          </p>
+          <div className='info-colums'>
+            <p className='info-colums__col'>humidity: {weatherData.main.humidity}</p>
+            <p className='info-colums__col'>Wind speed: {weatherData.wind.speed}</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/services/http.service.jsx
+++ src/services/http.service.jsx
@@ -6,7 +6,7 @@ export const getWeather = async (sityName) => {
   const API_key = config.API_key
 
   const {lat, lon} = await getGeolocation(sityName)
-  const apiEndPoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
+  const apiEndPoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_key}`
 
   const {data} = await axois.get(apiEndPoint)
   return data
