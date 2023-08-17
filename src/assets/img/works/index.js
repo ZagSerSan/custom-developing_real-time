ffc660282942fe50ef48cdc355ddee4503ed408c
ZagSerSan <yan.steys1997@gmail.com>
2023-08-17 12:13:54 +0200

    add masonry block

---
+++ src/assets/img/works/index.js
@@ -0,0 +1,21 @@
+import PHOTO_1 from './Photo1.png'
+import PHOTO_2 from './Photo2.png'
+import PHOTO_3 from './Photo3.png'
+import PHOTO_4 from './Photo4.png'
+import PHOTO_5 from './Photo5.png'
+import PHOTO_6 from './Photo6.png'
+import PHOTO_7 from './Photo7.png'
+import PHOTO_8 from './Photo8.png'
+import PHOTO_9 from './Photo9.png'
+
+export const photos = [
+  PHOTO_1,
+  PHOTO_2,
+  PHOTO_3,
+  PHOTO_4,
+  PHOTO_5,
+  PHOTO_6,
+  PHOTO_7,
+  PHOTO_8,
+  PHOTO_9
+]
---
+++ src/js/works.js
@@ -0,0 +1,26 @@
+// import { photos } from './../assets/img/works'
+// import Masonry from 'masonry-layout'
+
+// const grid = document.querySelector('.grid');
+
+// const gridItem = (src) => {
+//   const gridItem_el = document.createElement('div')
+//   gridItem_el.className = 'grid-item'
+//   gridItem_el.innerHTML = `
+//     <img src="${src}" alt="work photo">
+//   `
+//   return gridItem_el
+// }
+// photos.forEach(photo => {
+//   grid.append(gridItem(photo))
+// })
+
+// const msnry = new Masonry( grid, {
+//   // options...
+//   itemSelector: '.grid-item',
+//   columnWidth: 200,
+//   horizontalOrder: true
+// });
+
+
+
