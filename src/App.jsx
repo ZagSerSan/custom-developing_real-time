634071337350871c16dad12cf23b1b155387eb47
Serhii Zahorskyi <yan.steys1997@gmail.com>
2024-02-08 19:56:44 +0100

    i trying do it.. i`m so tired..

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import './App.css'
 import moment from 'moment'
 
 function App() {
-  const endTime = moment('2024-02-02 04:30:00')
-
+  const endTime = moment('2024-02-09 04:30:00')
   let [timerDate, setTimerDate] = useState(
   )
+
+  console.log(endTime.format("DD HH:mm:ss"))
+  timerDate && console.log(timerDate.format("DD HH:mm:ss"))
 
   let updateTimer = () => {
     if (Date.now() > endTime) {
       clearInterval(timer)
       console.log('Timer is over')
     } else {
-      setTimerDate(moment.utc(endTime.diff(Date.now())).format("DD:HH:mm:ss"))
+      // setTimerDate(moment.utc(endTime.diff(Date.now())).format("DD:HH:mm:ss"))
+      setTimerDate(moment(endTime - moment()))
     }
   }
   let timer = setInterval(updateTimer, 1000)
@@ -21,7 +24,8 @@ function App() {
   return (
     <>
       <div>
-        <p>{timerDate}</p>
+        {/* <p>{timerDate}</p> */}
+        <p>timerDate</p>
       </div>
     </>
   )
