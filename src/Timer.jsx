9f0ac35dfd04affe94004a606114a57be43dba8b
Serhii Zahorskyi <yan.steys1997@gmail.com>
2024-05-16 10:34:01 +0200

    by object

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
 const Timer = () => {
-  const endDate = new Date("2024-05-14T17:00:00")
+  const endDate = new Date("2024-05-16T17:00:00")
   const nowDate = Date.now()
   const duration = endDate.getTime() - nowDate
 
   const [time, setTime] = useState(duration)
+  // остановить выполнение кода если время = 0
+  if (duration <= 0) return console.log('timer is over')
 
+  // продолжить если больше нуля
   useEffect(() => {
     setTimeout(() => {
       setTime(duration - 1000)
@@ -19,14 +22,21 @@ const Timer = () => {
     let minutes = Math.floor(milisecundes / 1000 / 60 % 60)
     let secundes = Math.floor(milisecundes / 1000 % 60)
 
-    if (milisecundes <= 0 ) return 'timer is over!'
-
-    return `${days} days | ${hours} hours | ${minutes} minutes | ${secundes} secundes`
+    // if (milisecundes <= 0 ) return 'timer is over!'
+    // return `${days} days | ${hours} hours | ${minutes} minutes | ${secundes} secundes`
+    if (milisecundes <= 0 ) return {}
+    return { days, hours, minutes, secundes }
   }
+  
+  // console.log(formatTime(time))
+  let timeObj = formatTime(time)
 
   return (
     <div>
-      <p>{formatTime(time)}</p>
+      {Object.keys(timeObj).map(key => (
+        <p key={key}>{`${key}: ${timeObj[key]}`}</p>
+      ))}
+      {/* <p>{formatTime(time)}</p> */}
     </div>
   )
 }
