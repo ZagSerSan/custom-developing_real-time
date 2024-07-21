c6da61ab89c88f72ed229be2ed5d7c64ca362d16
Serhii Zahorskyi <yan.steys1997@gmail.com>
2024-07-21 17:23:55 +0200

    todo

--- src/dateStore.jsx
+++ src/dateStore.jsx
@@ -4,6 +4,7 @@ import { create } from 'zustand'
 
 const dateStore = create((set) => ({
   endDate: 0,
+  // todo - получить данные из селекта, отдать число для таймера
   // increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   // removeAllBears: () => set({ bears: 0 }),
 }))
