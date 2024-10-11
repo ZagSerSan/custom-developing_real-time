d4b3cfdd48ca09dd4ae1dd23f82f132305d4fbef
Serhii Zahorskyi <yan.steys1997@gmail.com>
2024-10-11 13:05:37 +0200

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import './App.css'
 import Timer from './components/Timer'
-import Select from './components/Select'
 
 function App() {
 
   return (
     <div className='app'>
-      <Select />
-      <hr/>
       <Timer />
     </div>
   )
