8dd1beccec0c97c22b53133bb1548012f6ac538d
Serhii Zahorskyi <yan.steys1997@gmail.com>
2025-03-06 13:57:09 +0100

    clean files

--- src/utils/formatTime.js
+++
@@ -1,9 +0,0 @@
-export const formatTime = (milisecundes) => {
-  let days = Math.floor(milisecundes / 60 / 60 / 24)
-  let hours = Math.floor(milisecundes / 60 / 60 % 24)
-  let minutes = Math.floor(milisecundes / 60 % 60)
-  let secundes = Math.floor(milisecundes % 60)
-
-  if (milisecundes <= 0 ) return false
-  return { days, hours, minutes, secundes }
-}
