13894bdfee0c41c121c5681881960f45b06374ce
Serhii Zahorskyi <yan.steys1997@gmail.com>
2025-03-06 15:13:10 +0100

    some fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,21 @@ import './App.css'
 import { useTranslation } from 'react-i18next'
 
 function App() {
-  const { t, i18n } = useTranslation(); // t — функция для получения перевода, i18n — для смены языка
+  const { i18n } = useTranslation(); // t — функция для получения перевода, i18n — для смены языка
+  const { t } = useTranslation('header')
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng); // Смена языка
   };
 
-  // welcome
-  // description
   return (
     <>
       <div>
-        <h1>{t('welcome')}</h1> {/* Перевод строки welcome */}
-        <p>{t('description')}</p> {/* Перевод строки description */}
+        <h1>{t('mainNav_home')}</h1> {/* Перевод строки welcome */}
 
         {/* Кнопки для смены языка */}
         <button onClick={() => changeLanguage('en')}>en</button>
         <button onClick={() => changeLanguage('ru')}>ru</button>
-        <button onClick={() => changeLanguage('pl')}>pl</button>
       </div>
     </>
   )
--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,21 +1,30 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-// Импортируем переводимые файлы
-import en from '../locales/en.json'
-import ru from '../locales/ru.json'
-import pl from '../locales/pl.json'
+// Подключаем переводимые файлы
+import enHeader from '../locales/en/header.json'
+// import enCart from './locales/en/cart.json'
+
+import ruHeader from '../locales/ru/header.json'
+// import ruCart from './locales/ru/cart.json'
 
 i18n
   .use(initReactI18next) // Подключаем React-обертку для i18next
   .init({
     resources: {
-      en: { translation: en }, // перевод на английский
-      ru: { translation: ru }, // перевод на русский
-      pl: { translation: pl }, // перевод на русский
+      en: {
+        header: enHeader,
+        // cart: enCart,
+      },
+      ru: {
+        header: ruHeader,
+        // cart: ruCart,
+      },
     },
     lng: 'en', // язык по умолчанию
     fallbackLng: 'en', // язык, на который будет происходить переключение в случае отсутствия перевода
+    ns: ['header'], // Добавляем все используемые namespaces
+    // defaultNS: 'common', // По умолчанию используем common.json
     interpolation: {
       escapeValue: false, // не экранировать значения
     },
